1dc63d65106aa5c6fc4a0875abd91b80f83d7a43
gnemek20 <gnemek20@gmail.com>
2023-03-19 00:56:55 +0900

    multer 제거, upload 페이지 제거

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-const multer = require('multer');
-const storage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, 'public/images');
-  },
-  filename(req, file, callback) {
-    callback(null, file.originalname);
-  }
-});
-const upload = multer({storage: storage});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -27,8 +16,5 @@ router.get('/image', (req, res) => {
 });
 
 /* POST Methods */
-router.post('/upload', upload.array('file'), (req, res) => {
-  res.send('success');
-});
 
 module.exports = router;
