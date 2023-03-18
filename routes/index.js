93c407efd7fbb3528f7907faca62bb86606acc9e
gnemek20 <gnemek20@gmail.com>
2023-03-19 00:26:58 +0900

    multer 추가, upload 페이지 추가

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, 'public/images');
+  },
+  filename(req, file, callback) {
+    callback(null, file.originalname);
+  }
+});
+const upload = multer({storage: storage});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Methods */
 router.get('/cwd', (req, res) => {
   res.send(process.cwd());
 });
 
 router.get('/image', (req, res) => {
   res.sendFile(`${process.cwd()}/public/images/chicken.png`);
+});
+
+/* POST Methods */
+router.post('/upload', upload.array('file'), (req, res) => {
+  res.send('success');
 });
 
 module.exports = router;
