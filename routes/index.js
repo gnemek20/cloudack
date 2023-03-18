2e02e3428faf89233c7dee5a3554fbcd29bfddab
gnemek20 <gnemek20@gmail.com>
2023-03-19 00:03:19 +0900

    /image, /cwd 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/cwd', (req, res) => {
+  res.send(process.cwd());
+});
+
+router.get('/image', (req, res) => {
+  res.sendFile(`${process.cwd()}/public/images/chicken.png`);
+});
+
 module.exports = router;
